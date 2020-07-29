4733677aeba627b2e596d18effdee4c94fd196a7
Lizbeth <ljgar24@gmail.com>
2020-07-29 11:43:24 -0700

    Initial Commit

---
+++ Simply Fit/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Simply Fit
+//
+//  Created by Apple on 7/29/20.
+//  Copyright © 2020 Liz. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Simply Fit/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Simply Fit
+//
+//  Created by Apple on 7/29/20.
+//  Copyright © 2020 Liz. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
