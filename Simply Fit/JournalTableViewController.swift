10c7edd0edfdd9eeaeb7164b59d909e96b2b734e
Jackie De Leon <jacqueline0deleon@gmail.com>
2020-07-30 14:13:50 -0700

    edit 8

--- Simply Fit/JournalTableViewController.swift
+++ Simply Fit/JournalTableViewController.swift
@@ -47,15 +47,17 @@ class JournalTableViewController: UITableViewController {
         return journalEntries.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        // Configure the cell...
+        let journalEntry = journalEntries[indexPath.row]
+        
+        cell.textLabel?.text = journalEntry.date
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
